2402d4245d2ce4d7932fc2b3ab62dcb994662d6f
Silvia Trejo <s.y.trejo26@gmail.com>
2022-06-06 20:13:55 -0400

    creating the initial app

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+function App(){
+  const [pages] = useState([
+
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <Nav
+        pages = {pages}
+        setCurrentPage = {setCurrentPage}
+        currentPage = {currentPage}
+        ></Nav>
+      </Header>
+
+      <main>
+        <Page currentPage = {currentPage}></Page>
+      </main>
+      <Footer />
+
     </div>
   );
 }
